d7c472d4be24064ef3351ee09a0aab2527b2c51e
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2024-06-14 17:49:26 -0300

    Ending Aula 01 - Frontend

---
+++ frontend/src/components/Chat.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export default function Chat(){
+    return(
+        <div className="bg-gray-900 px-4 pt-4 md:w-[15%] hidden md:flex rounded-md m-3 h-full">
+            <div className="relative h-full w-full">
+                <div className="bg-gray-950 rounded p-2">
+                    <div className="flex items-center text-pink-400 space-x-2">
+                        <span>Samuel Costa</span>
+                        <span>09:15</span>
+                    </div>
+                    
+                    <div className="mt-5 text-sm">
+                        <p>text</p>
+                    </div>
+                </div>
+
+                <form action="" className="absolute bottom-2 w-full">
+                    <div className="flex relative">
+                        <input type="text" className="px-3 py-2 bg-gray-950 rounded-md w-full" />
+                        <Image className="absolute right-2 top-2.5" src="/send.png" width={20} height={20} alt="send" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
